6c96b70253b30cadb5cb018b4d97b51e8e0c0c59
isak <isak.fogge1@gmail.com>
2023-02-04 02:35:27 +0100

    added click to start & game over text

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Just a small game to test out canvas in HTML.
 
+
+
--- js/main.js
+++ js/main.js
@@ -3,21 +3,43 @@ canvas.width = 480;
 canvas.height = 270;
 let ctx = canvas.getContext("2d");
 
+ctx.font = "30px Arial";
+ctx.textAlign = "center";
+ctx.fillText("Click to start", canvas.width/2, canvas.height/2); 
+
+canvas.addEventListener("click", startGame);
+
 let obstacles = [new Obstacle(480,135, "blue"), new Obstacle(720,135, "blue")];
 let player = new Player(10, 30, 30,30, "blue");
 
-let lastUpdate = Date.now();
+player.draw(ctx);
 
-canvas.addEventListener("click", () => player.jump())
+for (i of obstacles) {
+    i.draw(ctx);
+}
+
+let lastUpdate = 0
 
 let stopped = false;
 
-animate()
+function startGame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.removeEventListener("click", startGame);
+    canvas.addEventListener("click", () => player.jump())
+    lastUpdate = Date.now();
+    animate();
+}
 
 function animate() {
+
+    // Stop game
+    if (stopped) {
+        ctx.font = "30px Arial";
+        ctx.textAlign = "center";
+        ctx.fillText("Game Over!", canvas.width/2, canvas.height/2);
+        return;
+    }
     // Get delta time
-    if (stopped) return
-    
     let currentUpdate = Date.now();
     let dt = currentUpdate-lastUpdate;
     
